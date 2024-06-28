2a16d915e84a90e224a8ce9fa7d5c8897b1424b6
image <image@by.cx>
2024-06-28 23:27:59 +0800

    feat: plugin register & plugin manager

---
+++ bot/core.py
@@ -0,0 +1,4 @@
+class BotCore():
+    plugin_objects = {}
+    def __init__(self):
+        pass
--- plugins/dnd.py
+++ plugins/dnd.py
@@ -1,20 +1,18 @@
 from botpy.message import Message
 
-from plugins.plugin import Plugin
+from plugins.plugin import Plugin, KurumiPlugin
 
-
+@KurumiPlugin(name="DND")
 class DND(Plugin):
-    name = "DND"
-
     def register_commands(self):
         @self.cmd("dnd", "这是一个DND游戏的插件")
-        async def dnd(message, params=None):
+        async def dnd(self, message, params=None):
             content = f"你好，{message.author.username}，你的参数是：dnd {params}"
             await message.reply(content=content)
             return True
 
         @self.cmd("join", "加入游戏")
-        async def join(message, params=None):
+        async def join(self, message, params=None):
             content = f"你好，{message.author.username}，你的参数是：join {params}"
             await message.reply(content=content)
             return True
