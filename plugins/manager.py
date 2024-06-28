3f305bcb24e02bd4b8e74254372667c48b48e82a
Image <image@by.cx>
2024-06-28 19:19:31 +0800

    feat: 用户信息命令

--- plugins/manager.py
+++ plugins/manager.py
@@ -12,3 +12,16 @@ class Manager(Plugin):
             #await message.reply(content=content)
             return True
 
+        @self.cmd("me", "获取用户当前信息")
+        async def plugin(self, message: Message, params=None):
+            roles = []
+            for i in message.member.roles:
+                if i == '2':
+                    roles.append("管理员")
+                elif i == '4':
+                    roles.append("群主")
+                elif i == '5':
+                    roles.append("子频道管理员")
+            content = f"用户[{message.member.nick}-{message.author.id}]-{roles}\n"
+            await message.reply(content=content)
+            return True
