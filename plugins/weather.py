889c70b38fb995028a0de499eb5ed60c7ab84a10
image <image@by.cx>
2024-06-29 21:59:04 +0800

    美化输出格式

--- plugins/weather.py
+++ plugins/weather.py
@@ -3,7 +3,18 @@ import requests
 from plugins.plugin import Plugin, KurumiPlugin
 from llm.chat import Chat
 from llm.function_calling import *
+from botpy.types.message import Embed, EmbedField
+
 api_url = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={APIkey}"
+
+
+def f_to_c(f):
+    return round((f - 32.0) / 1.8, 1)
+
+
+def k_to_c(k):
+    return round(k - 273.15, 1)
+
 
 @KurumiPlugin(name="Weather")
 class Weather(Plugin):
@@ -15,16 +26,16 @@ class Weather(Plugin):
         """
 
         llm_chat = Chat(
-            base_url=self.core.config["OpenAI"]["base_url"], 
+            base_url=self.core.config["OpenAI"]["base_url"],
             api_key=self.core.config["OpenAI"]["api_key"],
             model="llama3-8b-8192",
             prompt=GET_LOCATION_PROMPT
-            )
-        
-        llm_res =llm_chat.simple_chat(content)
+        )
+
+        llm_res = llm_chat.simple_chat(content)
         llm_json = parse_to_json(llm_res.content)
         return llm_json
-            
+
     def register_commands(self):
         @self.cmd("/weather", "获取天气信息")
         async def weather(self, message, params=None):
@@ -35,10 +46,24 @@ class Weather(Plugin):
             if llm_json == None:
                 await message.reply(content="解析地址失败喵~")
                 return False
-            url = api_url.format(lat=llm_json["lat"], lon=llm_json["lon"], APIkey=self.core.config["OpenWeather"]["api_key"])
+            url = api_url.format(lat=llm_json["lat"], lon=llm_json["lon"],
+                                 APIkey=self.core.config["OpenWeather"]["api_key"])
 
             response = requests.get(url)
-            await message.reply(content=response.text)
+            weather_json = json.loads(response.text)
+            location = llm_json["location"]
+            weather_main = weather_json["weather"][0]["main"]
+            weather_embed = Embed(
+                title=f"{location} {weather_main}",
+                prompt=f"{location} {weather_main}",
+                fields=[
+                    EmbedField(
+                        name=f"实时温度: {k_to_c(weather_json['main']['temp'])}℃"),
+                    EmbedField(
+                        name=f"体感温度: {k_to_c(weather_json['main']['feels_like'])}"),
+                    EmbedField(
+                        name=f"当天温度: {k_to_c(weather_json['main']['temp_min'])} - {k_to_c(weather_json['main']['temp_max'])}"),
+                ],
+            )
+            await self.api.post_message(channel_id=message.channel_id, msg_id=message.id, embed=weather_embed)
             return True
-
-        
