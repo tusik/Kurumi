db36d640df56192ba4d4163daf00ce4c3bb46b25
Image <image@by.cx>
2024-06-30 15:09:06 +0800

    change: llm prompt & generate with image

--- plugins/weather.py
+++ plugins/weather.py
@@ -4,8 +4,9 @@ from plugins.plugin import Plugin, KurumiPlugin
 from llm.chat import Chat
 from llm.function_calling import *
 from botpy.types.message import Embed, EmbedField
+from utils.weather_help import draw_today
 
-api_url = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={APIkey}"
+api_url = "https://api.openweathermap.org/data/2.5/weather?q={city},{country}&appid={APIkey}"
 
 
 def f_to_c(f):
@@ -21,7 +22,7 @@ class Weather(Plugin):
     def get_location_llm(self, content):
         GET_LOCATION_PROMPT = """
         你是文本提取工具，你能提取以下内容中的地址，并输出经纬度到以下json格式中:
-        {\"lat\":地址的纬度,\"lon\":地址的经度,\"location\":地址的英文名称}.
+        {\"city name\":城市英文名,\"state code\":州代码,\"country code\":国家代码}
         最后只输出json内容给用户
         """
 
@@ -39,31 +40,21 @@ class Weather(Plugin):
     def register_commands(self):
         @self.cmd("/weather", "获取天气信息")
         async def weather(self, message, params=None):
-            if params == None:
+            plugin_config = self.core.config["plugins"]["weather"]
+            if params is None:
                 await message.reply(content="你还没告诉我哪里喵~")
                 return False
             llm_json = self.get_location_llm(params)
-            if llm_json == None:
+            if llm_json is None:
                 await message.reply(content="解析地址失败喵~")
                 return False
-            url = api_url.format(lat=llm_json["lat"], lon=llm_json["lon"],
-                                 APIkey=self.core.config["OpenWeather"]["api_key"])
+            url = api_url.format(city=llm_json["city name"], country=llm_json["country code"],
+                                 APIkey=plugin_config["OpenWeather"]["api_key"])
 
             response = requests.get(url)
-            weather_json = json.loads(response.text)
-            location = llm_json["location"]
-            weather_main = weather_json["weather"][0]["main"]
-            weather_embed = Embed(
-                title=f"{location} {weather_main}",
-                prompt=f"{location} {weather_main}",
-                fields=[
-                    EmbedField(
-                        name=f"实时温度: {k_to_c(weather_json['main']['temp'])}℃"),
-                    EmbedField(
-                        name=f"体感温度: {k_to_c(weather_json['main']['feels_like'])}"),
-                    EmbedField(
-                        name=f"当天温度: {k_to_c(weather_json['main']['temp_min'])} - {k_to_c(weather_json['main']['temp_max'])}"),
-                ],
-            )
-            await self.api.post_message(channel_id=message.channel_id, msg_id=message.id, embed=weather_embed)
+            image = draw_today(response.text, plugin_config["cache_path"])
+            if image is None:
+                await self.api.post_message(channel_id=message.channel_id, msg_id=message.id, content="绘制图形失败喵~")
+            else:
+                await self.api.post_message(channel_id=message.channel_id, msg_id=message.id, file_image=image)
             return True
