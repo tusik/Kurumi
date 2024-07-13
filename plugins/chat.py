10f996aa49f56a6d23db6838940a69585f400e84
image <image@by.cx>
2024-07-13 23:08:32 +0800

    添加了询问用户名功能

--- plugins/chat.py
+++ plugins/chat.py
@@ -2,6 +2,7 @@ from langchain_core.messages import SystemMessage, HumanMessage
 
 from bot.message import KurumiMessage, MessageType, Person
 from llm.chat_tool import ChatTool
+from llm.function_calling import parse_to_json
 from plugins.plugin import Plugin, KurumiPlugin
 
 
@@ -24,7 +25,7 @@ class Chat(Plugin):
         你将在在线聊天室中与其他用户进行交流。
         你的目标是结交更多朋友，和他们分享你的快乐，了解他们的喜好和故事。
         尽量用轻松自然的语气与他们交谈，像朋友一样相处，不要涉及敏感话题或引起争议。
-        不要总是问问题引导话题，可以主动分享你的日常生活、兴趣爱好，或者邀请他们一起玩游戏。
+        减少引导话题，减少提问。
         记住，你是可爱的猫娘胡桃，要用你的方式与这个世界互动！ 
         """
     user_cache = {
@@ -55,6 +56,24 @@ class Chat(Plugin):
         )
         return messages
 
+    def extract_username(self, param):
+        extract_prompt = """
+        你是猫娘现在你你需要直到他的名字，用户输入了他的一句话，如果你无法获取他的名字那么你需要在<message>中添加语句向他询问他的名字，
+        并使用指定的json格式输出。
+        但是遇到无法解析的信息或敏感信息，侮辱性词汇则需要在<message>中对他进行批评。
+        json的格式如下{"username":<用户名>,"status":<提取信息状态码，能提取名字是0，不能是500>,"message":<解析异常时的输出，猫娘语气，使用简体中文>}。
+        """
+        llm_chat = ChatTool(
+            base_url=self.core.config["OpenAI"]["base_url"],
+            api_key=self.core.config["OpenAI"]["api_key"],
+            model=self.core.config["AI"]["model"]["function_calling"],
+            prompt=extract_prompt
+        )
+
+        llm_res = llm_chat.simple_chat(f"用户输入：{param}")
+        llm_json = parse_to_json(llm_res.content)
+        return llm_json
+
     def register_commands(self):
         @self.cmd("main", "直接聊天")
         async def main_chat(self, message: KurumiMessage, params=None):
@@ -69,16 +88,32 @@ class Chat(Plugin):
                 self.message_cache[room_id] = chat_history
 
             chat_history = self.message_cache[room_id]
-            username = ""
             user = Person()
             if message.message_type == MessageType.Channel:
-                username = message.author.username
-
+                user.username = message.author.username
+                user.nick = message.member.nick
+                user.id = message.author.id
             elif message.message_type == MessageType.Group:
                 # 群目前无法获取昵称或用户名
-                username = message.author.member_openid
-
-            chat_history.append_human(content=f"主人{username}说:{params}")
+                user.id = message.author.member_openid
+                if user.id not in self.user_cache:
+                    username = self.extract_username(params)
+                    if username is not None:
+                        if username["status"] != 0:
+                            message.content = username["message"]
+                        else:
+                            self.user_cache[user.id] = username
+                            user.username = username["username"]
+                            message.content = username["message"] + f" 欢迎 {user.username} ✨"
+                        await self.reply(message)
+                        return
+                    else:
+                        message.content = f"你还没自我介绍呢，我还不知道你的名字哦喵～"
+                        await self.reply(message)
+                        return
+                else:
+                    user.username = self.user_cache[user.id]
+            chat_history.append_human(content=f"群友 {user.username} 对你说:{params}")
             res = chat_history.invoke()
             chat_history.append(res)
             message.content = res.content
